711a2d36e0499c41dcbd9a92e3bce800ee1af9a3
Yunyang <sunnycloudyang@outlook.com>
2022-08-22 23:55:07 +0800

    v-1.3.3 latest version

---
+++ scripts/BouncePro.js
@@ -0,0 +1,405 @@
+//GoodNullName changed something magically
+class Ball {
+    constructor(x, y, velX, velY, color, r) {
+        this.x = x;
+        this.y = y;
+        this.vx = velX;
+        this.vy = velY;
+        this.color = color;
+        this.radius = r;
+        this.mess = rou * r ** 3;
+        this.fri_ax = 0;
+        this.ax = 0;
+        this.ay = 0;
+        this.last_x = 0;
+        this.last_y = 0;
+        this.stop = false;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+
+    update() {
+        if (this.x <= this.radius) {
+            //rebound at left
+            this.x = this.radius;
+            this.vx = Math.abs(recovery * this.vx);
+        }
+        else if (this.x >= width - this.radius) {
+            //rebound at right
+            this.x = width - this.radius;
+            this.vx = -Math.abs(recovery * this.vx);
+        }
+
+        if (this.y <= this.radius) {
+            //always rebound at ceil
+            this.y = this.radius;
+            this.vy = Math.abs(recovery * this.vy);
+        }
+        else if (this.y >= height - this.radius) {
+            //at floor
+            if (!gravity) {
+                this.y = height - this.radius;
+                this.vy = -Math.abs(recovery * this.vy);
+            }
+            else {
+                if (!energy_loss) {
+                    this.y = height - this.radius;
+                    this.vy += gy;
+                    this.vy = -Math.abs(gy * Math.round(this.vy / gy));
+                }
+                else if (this.vy ** 2 > 0.4 * this.radius * gy) {
+                    this.y = height - this.radius;
+                    this.vy += gy;
+                    this.vy = -Math.abs(recovery * gy * Math.floor(this.vy / gy));
+                }
+                else {
+                    this.y = height - this.radius;
+                    this.vy = 0;
+                    if (energy_loss) {
+                        if (Math.abs(this.vx) > mu_floor)
+                            this.fri_ax = -mu_floor * (this.vx / Math.abs(this.vx));
+                        else {
+                            this.vx = 0;
+                            this.fri_ax = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (this.vy == 0 && this.y == height - this.radius) {
+            //acceleration of x
+            this.vx += this.fri_ax;
+        }
+
+        this.stop = (this.x == this.radius || this.x == width - this.radius
+            || this.y == this.radius || this.y == height - this.radius)
+            && Math.abs(this.vx) < gx + 0.05
+            && Math.abs(this.vy) < gy + 0.05;
+
+        this.last_x = this.x;
+        this.last_y = this.y; //choose
+        this.vx += this.ax + (this.stop ? 0 : gx);
+        this.vy += this.ay + (this.stop ? 0 : gy);
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+
+    F_grav(serialNumber) {
+        let F_g = 0;
+        let F_gmax = (g_uni * this.mess * balls_valumn[serialNumber].mess) /
+            ((this.radius + balls_valumn[serialNumber].radius) ** 2);
+
+        if (Math.abs(balls_valumn[serialNumber].x - this.x) > 1
+            || Math.abs(balls_valumn[serialNumber].y - this.y) > 1) {
+            F_g = (g_uni * this.mess * balls_valumn[serialNumber].mess) /
+                this.distance(serialNumber);
+        }
+        return F_g < F_gmax ? F_g : F_gmax;
+    } //compute the acceleration of gravation
+
+    position_angel(serialNumber) {
+        if (Math.abs(balls_valumn[serialNumber].x - this.x) > 1
+            || Math.abs(balls_valumn[serialNumber].y - this.y) > 1) {
+            if (balls_valumn[serialNumber].x > this.x) {
+                return Math.atan(
+                    (this.y - balls_valumn[serialNumber].y) /
+                    (this.x - balls_valumn[serialNumber].x));
+            }
+            else if (balls_valumn[serialNumber].x < this.x) {
+                return (
+                    Math.PI +
+                    Math.atan(
+                        (this.y - balls_valumn[serialNumber].y) /
+                        (this.x - balls_valumn[serialNumber].x)));
+            }
+            else {
+                return this.y > balls_valumn[serialNumber].y ? -Math.PI / 2 : Math.PI / 2;
+            }
+        }
+        else
+            return 0;
+    } //compute the angel between ball[serialNumber]
+
+    gravAround(me) {
+        if (universe_mode) {
+            let f_g = 0;
+            let cos_t = 0;
+            let sin_t = 0;
+            for (var i = me + 1; i < cnt; i++) {
+                if (merge_mode && this.isInsideMe(balls_valumn[i].x, balls_valumn[i].y, 1.1, 2)) {
+                    try {
+                        EatBall(me, i);
+                    }
+                    catch (error) {
+                        console.log("Ignored " + error.message);
+                    }
+                }
+                else {
+                    f_g = this.F_grav(i);
+                    cos_t = Math.cos(this.position_angel(i));
+                    sin_t = Math.sin(this.position_angel(i))
+                    this.ax += (f_g / this.mess) * cos_t;
+                    this.ay += (f_g / this.mess) * sin_t;
+                    balls_valumn[i].ax -= (f_g / balls_valumn[i].mess) * cos_t;
+                    balls_valumn[i].ay -= (f_g / balls_valumn[i].mess) * sin_t;
+                }
+            }
+        }
+    } //compute a of gravation in x and y in total
+
+    distance(serialNumber) {
+        //squared distance to balls[serialNumber]
+        return (this.x - balls_valumn[serialNumber].x) ** 2 +
+            (this.y - balls_valumn[serialNumber].y) ** 2;
+    }
+
+    collideWith(i) {
+        for (var j = i + 1; j < cnt; j++) {
+            CheckCollision(this, balls_valumn[j]);
+        }
+    }
+
+    isInsideMe(event_x, event_y, ratio = 1, dist = 0) {
+        return (this.x - event_x) ** 2 + (this.y - event_y) ** 2 <= (this.radius * ratio + dist) ** 2;
+    } //choose
+}
+
+function CheckCollision(ball0, ball1) {
+    let dx = ball1.x - ball0.x;
+    let dy = ball1.y - ball0.y;
+    let dist = Math.sqrt(dx * dx + dy * dy);
+    // 检测冲突
+    if (dx * (ball0.vx - ball1.vx) + dy * (ball0.vy - ball1.vy) > 0 &&
+        dist <= ball0.radius + ball1.radius) {
+        let angle = Math.atan2(dy, dx);
+        let sin = Math.sin(angle);
+        let cos = Math.cos(angle);
+
+        // boll0旋转后小球的位置
+        let pos0 = { x: 0, y: 0 };
+        // boll1旋转后小球的位置
+        let pos1 = rotate(dx, dy, sin, cos, true);
+        // ball0旋转后的速度
+        let vel0 = rotate(ball0.vx, ball0.vy, sin, cos, true);
+        // ball1旋转后的速度
+        let vel1 = rotate(ball1.vx, ball1.vy, sin, cos, true);
+
+        let vxTotal = vel0.x - vel1.x;
+        vel0.x = ((ball0.mess - ball1.mess) * vel0.x + 2 * ball1.mess * vel1.x) / (ball0.mess + ball1.mess);
+        vel1.x = vxTotal + vel0.x;
+
+        // 两小球的x坐标加上速度 计算出新的x坐标
+        pos0.x += vel0.x;
+        pos1.x += vel1.x;
+        // 相对于ball0原点为圆心时 旋转回去的坐标
+        let pos0F = rotate(pos0.x, pos0.y, sin, cos, false);
+        let pos1F = rotate(pos1.x, pos1.y, sin, cos, false);
+        // 相对于 原来的坐标
+        ball1.x = ball0.x + pos1F.x;
+        ball1.y = ball0.y + pos1F.y;
+        ball0.x = ball0.x + pos0F.x;
+        ball0.y = ball0.y + pos0F.y;
+        // 速度旋转回去
+        let vel0F = rotate(vel0.x, vel0.y, sin, cos, false);
+        let vel1F = rotate(vel1.x, vel1.y, sin, cos, false);
+        ball0.vx = vel0F.x;
+        ball0.vy = vel0F.y;
+        ball1.vx = vel1F.x;
+        ball1.vy = vel1F.y;
+    }
+}
+
+let chosed = cnt;
+function ChooseBall(ev) {
+    ev.preventDefault();
+    let mouse_down = getEventPosition(ev);
+    for (var j = 0; j < cnt; j++) {
+        if (balls_valumn[j].isInsideMe(mouse_down.x, mouse_down.y)) {
+            balls_valumn[j].vx = 0;
+            balls_valumn[j].vy = 0;
+            chosed = j;
+        }
+    }
+    if (chosed < cnt) {
+        document.addEventListener("ontouchmove", MoveBall, { passive: false });
+        document.onmousemove = MoveBall;
+    }
+}
+
+function MoveBall(ev) {
+    ev.preventDefault();
+    let x_pro = ev.layerX;
+    let y_pro = ev.layerY;
+    let maxX = width - balls_valumn[chosed].radius;
+    let maxY = height - balls_valumn[chosed].radius;
+    if (x_pro < balls_valumn[chosed].radius) {
+        x_pro = balls_valumn[chosed].radius;
+    }
+    else if (x_pro > maxX) {
+        x_pro = maxX;
+    }
+    if (y_pro < balls_valumn[chosed].radius) {
+        y_pro = balls_valumn[chosed].radius;
+    }
+    else if (y_pro > maxY) {
+        y_pro = maxY;
+    }
+
+    balls_valumn[chosed].x = x_pro;
+    balls_valumn[chosed].y = y_pro;
+    balls_valumn[chosed].vx = x_pro - balls_valumn[chosed].last_x;
+    balls_valumn[chosed].vy = y_pro - balls_valumn[chosed].last_y;
+
+    document.onmouseup = ReleaseBall;
+    document.addEventListener("ontouchend", ReleaseBall, { passive: false });
+    document.addEventListener("ontouchcancel", ReleaseBall, { passive: false });
+};
+
+function ReleaseBall() {
+    document.onmousemove = "";
+    document.ontouchmove = "";
+    chosed = cnt;
+};
+
+function EatBall(num_ball0, num_ball1) {
+    if (balls_valumn[num_ball1].radius > balls_valumn[num_ball0].radius) {
+        num_ball0 += num_ball1;
+        num_ball1 = num_ball0 - num_ball1;
+        num_ball0 = num_ball0 - num_ball1;
+    }
+    balls_valumn[num_ball0].mess += balls_valumn[num_ball1].mess;
+    balls_valumn[num_ball0].radius = (balls_valumn[num_ball0].mess / rou) ** (1 / 3);
+
+    balls_valumn[num_ball0].vx += (balls_valumn[num_ball1].mess / balls_valumn[num_ball0].mess) * (balls_valumn[num_ball1].vx - balls_valumn[num_ball0].vx);
+
+    balls_valumn[num_ball0].vy += (balls_valumn[num_ball1].mess / balls_valumn[num_ball0].mess) * (balls_valumn[num_ball1].vy - balls_valumn[num_ball0].vy);
+
+    balls_valumn.splice(num_ball1, 1);
+    cnt--;
+}
+
+let last_time = 0;
+function DeviceMove(ev) {
+    var cur_time = new Date().getTime();
+    if (cur_time - last_time > 16) {
+        last_time = cur_time;
+        let accl = ev.acceleration;
+        let ax = accl.x;
+        let ay = accl.y;
+        if (ax != null && ay != null) {
+            ax = (Math.abs(ax) < 2 ? 0 : Math.abs(ax) > 20 ? (ax / Math.abs(ax)) * 20 : ax);
+            ay = (Math.abs(ay) < 2 ? 0 : Math.abs(ay) > 20 ? (ay / Math.abs(ay)) * 20 : ay);
+            for (var i = 0; i < cnt; i++) {
+                balls_valumn[i].ax -= ax / 5;
+                balls_valumn[i].ay += ay / 5;
+            }
+            console.log("Shaking balls...", ax, ay);
+        }
+    }
+    // try {
+    //     gx = loc_g_mode && ev.accelerationIncludingGravity.x ? ev.accelerationIncludingGravity.x : 0;
+    //     gy = loc_g_mode && ev.accelerationIncludingGravity.y ? ev.accelerationIncludingGravity.y : 0;
+    //     console.log(gx, gy);
+    // } catch (error) {
+    //     console.log(error);
+    // }
+}
+
+function DeviceRotate(ev) {
+    console.log("rotate: " + ev.gamma);
+}
+
+function GetAmount() {
+    //adjust the number of balls
+    let new_number = document.getElementById("number").value;
+    if (new_number > 0 && new_number <= max_balls) {
+        if (new_number > cnt) {
+            NewBalls(new_number - cnt);
+        }
+        else
+            balls_valumn.splice(0, cnt - new_number);
+        cnt = new_number;
+        document.getElementById("number").value = "";
+    }
+    else
+        alert("Invalid number! Balls must be less than " + max_balls + " and not null.");
+}
+
+function SumOfBalls() {
+    cnt = balls_valumn.length;
+    for (var i = 0; i < balls_valumn.length; i++)
+        if (!(balls_valumn[i].x > 0 - balls_valumn[i].radius &&
+            balls_valumn[i].x < width + balls_valumn[i].radius &&
+            balls_valumn[i].y > 0 - balls_valumn[i].radius &&
+            balls_valumn[i].y < height + balls_valumn[i].radius)) {
+            balls_valumn.splice(i, 1);
+            cnt--;
+        }
+    return cnt;
+}
+
+function NewBalls(amount) {
+    //add some new balls
+    for (var i = 0; i < amount; i++) {
+        let r_new = random_int(min_r, max_r);
+        let x_new = random_int(r_new, width - r_new);
+        let y_new = random_int(r_new, height - r_new);
+        let velX_new = random(-max_vx, max_vx);
+        let velY_new = random(-max_vy, max_vy);
+        let color_new = random_color();
+        let b_new = new Ball(x_new, y_new, velX_new, velY_new, color_new, r_new);
+        balls_valumn.push(b_new);
+    }
+}
+
+function DrawRect() {
+    if (day_mode)
+        ctx.strokeStyle = ctx.fillStyle = hex2rgba(day_color, 0.55 + fuzzy);
+    else
+        ctx.strokeStyle = ctx.fillStyle = hex2rgba(night_color, 0.55 + fuzzy);
+    ctx.lineJoin = "round";
+    ctx.lineWidth = 20;
+    ctx.fillRect(20, 20, width - 40, height - 40);
+    ctx.strokeRect(10, 10, width - 20, height - 20);
+}
+
+for (var i = 0; i < number_of_balls; i++) {
+    let r = random_int(min_r, max_r);
+    let x = random_int(r, width - r);
+    let y = random_int(r, height - r);
+    let velX = random(-max_vx, max_vx);
+    let velY = random(-max_vy, max_vy);
+    let color = random_color();
+    let b = new Ball(x, y, velX, velY, color, r);
+    balls_valumn.push(b);
+}
+
+setInterval(() => {
+    cnt_of_balls_now.innerHTML = "Number of balls now: "
+        + SumOfBalls();
+}, universe_mode ? 50 : 250);
+myCanvas.onmousedown = ChooseBall;
+myCanvas.addEventListener("ontouchstart", ChooseBall, { passive: false });
+
+function movingLoop() {
+    DrawRect();
+    for (var i = 0; i < cnt; i++) {
+        balls_valumn[i].collideWith(i);
+    }
+    for (var i = 0; i < cnt; i++) {
+        balls_valumn[i].gravAround(i);
+    }
+    for (var i = 0; i < cnt; i++) {
+        balls_valumn[i].update();
+        balls_valumn[i].draw();
+        balls_valumn[i].ax = balls_valumn[i].ay = 0;
+    }
+    requestAnimationFrame(movingLoop);
+}
+movingLoop();
---
+++ scripts/settings.js
@@ -0,0 +1,284 @@
+const cans = document.querySelector("canvas");
+const ctx = cans.getContext("2d");
+let myCanvas = document.getElementById("myCanvas");
+let cnt_of_balls_now = document.getElementById("cnt");
+let width = cans.width = window.innerWidth - 16;
+let height = cans.height = window.innerHeight - 30;
+let balls_valumn = [];
+let min_r = 7;
+let max_r = 20;
+let max_balls = 25 * Math.floor(width * height / (1500 * (min_r + max_r)));
+let number_of_balls = (width * height < 300000 ? 50 : 100); //default balls amount
+let max_vx = 3;
+let max_vy = 3;
+let default_gy = 0.4; //acceleration of gravity
+let g_uni = 0.667; //the gravitational constant
+let mu_floor = 0.01;
+let gx = 0;
+let gy = 0;
+let rou = 1; //density of ball
+let cnt = 0;;
+let recovery = 1;
+let fuzzy = 0.3;
+let night_mode = true; //default mode
+let day_mode = false;
+let universe_mode = false;
+let merge_mode = true;
+let gravity = false;
+let loc_g_mode = false;
+let energy_loss = false;
+let shake_mode = true;
+let night_color = "#2a273c";
+let day_color = "#feffe6";
+let bg_color = night_mode ? night_color : day_color;
+
+let title = document.getElementById("start");
+title.onclick = () => {
+    alert("Welcome to Version 1.3.3, now you can shake the balls. Double click 'Loss' button to open/close this mode.");
+    // and double click 'Gravity' to dis / enable ground pointing in gravity mode
+};
+console.log("Welcome to Version 1.3.3 with shake mode. Shake your phone to see the changes.");
+
+let day_btn = document.getElementById("day_mode");
+let night_btn = document.getElementById("night_mode");
+let cust_btn = document.getElementById("custom");
+
+night_btn.onclick = function () {
+    //codes below set the modes.(in an awful way)
+    night_mode = true;
+    day_mode = false;
+    night_btn.style.backgroundColor = "black";
+    day_btn.style.backgroundColor = "rgb(225,225,225)";
+    bg_color = night_color;
+};
+night_btn.ondblclick = function () {
+    fuzzy = -fuzzy;
+}
+
+day_btn.onclick = function () {
+    day_mode = true;
+    night_mode = false;
+    night_btn.style.backgroundColor = "dimgray";
+    day_btn.style.backgroundColor = "white";
+    bg_color = day_color;
+};
+day_btn.ondblclick = function () {
+    fuzzy = -fuzzy;
+}
+
+let grav_btn = document.getElementById("gravity");
+let engy_btn = document.getElementById("energy_loss");
+let uni_btn = document.getElementById("universe_mode");
+
+let GravClick = null;
+grav_btn.onclick = function () {
+    clearTimeout(GravClick);
+    GravClick = setTimeout(() => {
+        gravity = !gravity;
+        if (gravity && (min_r + max_r) * cnt > 1.1 * width) {
+            alert("There are too many balls for the floor to contain!")
+            gravity = false;
+        }
+        gy = gravity ? default_gy : 0;
+        grav_btn.style.color = gravity ? "white" : "black";
+        grav_btn.style.backgroundColor = gravity ? "purple" : "rgba(225,225,225,1)";
+    }, 200);
+};
+grav_btn.ondblclick = function () {
+    clearTimeout(GravClick);
+    //loc_g_mode = !loc_g_mode;
+    gx = 0;
+    gy = loc_g_mode ? 0 : gravity ? default_gy : 0;
+    //loc_g_mode ? alert("Local gravity mode is open.") : alert("Local gravity mode is close.");
+}
+
+let LossClick = null;
+engy_btn.onclick = function () {
+    clearTimeout(LossClick);
+    LossClick=setTimeout(() => {
+        if (!energy_loss) {
+            energy_loss = true;
+            recovery = 0.85;
+            engy_btn.style.color = "white";
+            engy_btn.style.backgroundColor = "purple";
+        }
+        else {
+            energy_loss = false;
+            recovery = 1;
+            engy_btn.style.color = "black";
+            engy_btn.style.backgroundColor =
+                "rgba(225,225,225,1)";
+        }
+    }, 200);
+};
+engy_btn.ondblclick = function () {
+    clearTimeout(LossClick);
+    shake_mode = !shake_mode;
+    shake_mode ? alert("Shake mode is open.") : alert("Shake mode is close.");
+    shake_mode ? CheckMotion() : window.ondevicemotion = "";
+}
+
+let UniClick = null;
+uni_btn.onclick = function () {
+    clearTimeout(UniClick);
+    UniClick = setTimeout(() => {
+        if (!universe_mode) {
+            let conf;
+            cnt * g_uni > 10 ? conf = confirm("NOT SUGGESTED to open this mode with too much balls.\n" + "It'll be in a MESS and balls may FLY OUT.\n" + "ARE YOU SURE TO GO ON?") : conf = true;
+            if (conf) {
+                universe_mode = true;
+                uni_btn.style.color = "white";
+                uni_btn.style.backgroundColor = "#002e63";
+            }
+        }
+        else {
+            universe_mode = false;
+            uni_btn.style.color = "black";
+            uni_btn.style.backgroundColor = "rgb(225,225,225)";
+        }
+    }, 200);
+};
+uni_btn.ondblclick = function () {
+    clearInterval(UniClick);
+    merge_mode = !merge_mode;
+    merge_mode ? alert("Merge mode is open.") : alert("Merge mode is close.");
+}
+
+document.getElementById("number").onkeydown = function (ev) {
+    if (ev.key === 'Enter') {
+        GetAmount();
+    }
+}
+
+window.onresize = () => {
+    width = cans.width = window.innerWidth - 16;
+    height = cans.height = window.innerHeight - 30;
+    CheckSize();
+}
+
+setTimeout(() => {
+    CheckSize();
+    CheckMotion();
+}, 1500);
+
+function CheckSize() {
+    let input_num = document.getElementById("number");
+    if (width <= 520) {
+        day_btn.innerHTML = "Day";
+        night_btn.innerHTML = "Night";
+        cust_btn.innerHTML = "Cust.";
+        grav_btn.innerHTML = "Grav.";
+        engy_btn.innerHTML = "Loss";
+        uni_btn.innerHTML = "Univ.";
+        title.setAttribute("style", "font-size:28px");
+        input_num.setAttribute("style", "width:50px");
+    }
+    else {
+        day_btn.innerHTML = "Day mode";
+        night_btn.innerHTML = "Night mode";
+        cust_btn.innerHTML = "Custom";
+        grav_btn.innerHTML = "Gravity";
+        engy_btn.innerHTML = "Energy loss";
+        uni_btn.innerHTML = "Universe mode";
+        title.setAttribute("style", "font-size:44px");
+        input_num.setAttribute("style", "width:88px");
+    }
+    max_balls = 25 * Math.floor(width * height / (1500 * (min_r + max_r)));
+    max_balls = max_balls < 700 ? max_balls : 700;
+    input_num.setAttribute("placeholder", "1-" + max_balls);
+}
+
+function CheckMotion() {
+    if (shake_mode && window.DeviceMotionEvent) {
+        window.ondevicemotion = DeviceMove;
+        window.ondeviceorientation = DeviceRotate;
+    }
+    else {
+        alert("Device move sensor is not supported.");
+    }
+}
+
+function getEventPosition(ev) {
+    return { x: ev.layerX, y: ev.layerY };
+} //choose
+
+cust_btn.onclick = UserDef;
+let set_menu = document.getElementById("user_settings");
+function UserDef() {
+    set_menu.style.display == "inline-flex" ? set_menu.style.display = "none" : set_menu.style.display = "inline-flex";
+    document.getElementById("val").value = document.getElementById("g_const").value = g_uni;
+    document.getElementById("color").value = bg_color;
+}
+
+
+document.getElementById("save_set").onclick = () => {
+    let user_min_r = document.getElementById("min_r").value;
+    let user_max_r = document.getElementById("max_r").value;
+    let user_max_vx = document.getElementById("max_vx").value;
+    let user_max_vy = document.getElementById("max_vy").value;
+    let user_g = document.getElementById("g_const").value;
+    let user_color = document.getElementById("color").value;
+    if (user_min_r &&
+        user_min_r <=
+        max_r) {
+        min_r = Number(user_min_r);
+        console.log("Read min r: " + min_r);
+    }
+    else if (user_min_r) {
+        alert("Invalid min radius! Must be no bigger than " + max_r);
+    }
+    if (user_max_r &&
+        user_max_r >=
+        min_r) {
+        max_r = Number(user_max_r);
+        console.log("Read max r: " + max_r);
+    }
+    else if (user_max_r) {
+        alert("Invalid max radius! Must be no less than " + min_r);
+    }
+    if (user_min_r || user_max_r) {
+        if (max_r - min_r > cnt / 5) {
+            for (var i = 0; i < cnt; i++) {
+                balls_valumn[i].radius = random_int(min_r, max_r);
+                balls_valumn[i].mess = rou * balls_valumn[i].radius ** 3;
+            }
+        }
+        else
+            for (var i = 0; i < cnt; i++) {
+                balls_valumn[i].radius = random(min_r, max_r);
+                balls_valumn[i].mess = rou * balls_valumn[i].radius ** 3;
+            }
+        CheckSize();
+    }
+
+    if (user_max_vx) {
+        max_vx = Number(user_max_vx);
+        console.log("Read max vx: " + max_vx);
+    }
+    if (user_max_vy) {
+        max_vy = Number(user_max_vy);
+        console.log("Read max vy: " + max_vy);
+    }
+    if (user_max_vx || user_max_vy) {
+        for (var i = 0; i < cnt; i++) {
+            balls_valumn[i].vx = random(-max_vx, max_vx);
+            balls_valumn[i].vy = random(-max_vy, max_vy);
+        }
+    }
+    g_uni = Number(user_g);
+    day_mode ? day_color = user_color : night_color = user_color;
+    console.log("Current settings: ", min_r, max_r, max_vx, max_vy, g_uni, bg_color);
+    document.getElementById("min_r").value = '';
+    document.getElementById("max_r").value = '';
+    document.getElementById("max_vx").value = '';
+    document.getElementById("max_vy").value = '';
+    set_menu.style.display = "none";
+}
+
+document.getElementById("cancel_set").onclick = () => {
+    document.getElementById("min_r").value = '';
+    document.getElementById("max_r").value = '';
+    document.getElementById("max_vx").value = '';
+    document.getElementById("max_vy").value = '';
+    set_menu.style.display = "none";
+}
