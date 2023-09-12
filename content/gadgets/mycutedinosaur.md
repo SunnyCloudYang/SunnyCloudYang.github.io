a483cf7456ed21b5ae72e7a7fef50f5c9d086c78
Yunyang <sunnycloudyang@outlook.com>
2023-09-13 01:50:32 +0800

    New: add my cute dinosaur

---
+++ content/gadgets/mycutedinosaur.md
@@ -0,0 +1,25 @@
+---
+author: Yang
+date: "2023-09-12T18:15:33+08:00"
+lastmod: "2023-09-13T00:47:37+08:00"
+description: ""
+title: "社恐龙小绿"
+summary: "这里面是我的爱龙小绿，除了妈妈，请不要和他说话"
+tags: ["gadgets"]
+categories: "gadgets"
+# cover: 
+#    image: "images/.jpg"
+draft: true
+comments: true
+hideMeta: false
+searchHidden: false
+ShowBreadCrumbs: true
+ShowReadingTime: false
+---
+
+<div id="dinosaurdiv">
+</div>
+
+<link rel="stylesheet" href="/css/mycutedinosaur.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/110/three.min.js"></script>
+<script src="/js/mycutedinosaur.js"></script>
---
+++ static/js/mycutedinosaur.js
@@ -0,0 +1,451 @@
+'use strict';
+
+let width,
+    height,
+    marginWidth = 80,
+    marginHeight = 400;
+
+let scene,
+    camera,
+    renderer,
+    effect,
+    geometry,
+    material,
+    world,
+    night = false;
+
+let dinosaur,
+    cloud,
+    sky,
+    tree;
+
+function init() {
+    width = window.innerWidth - marginWidth;
+    height = window.innerHeight - marginHeight;
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    camera.position.y = 1;
+    camera.position.z = -5;
+    camera.lookAt(scene.position);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+    // effect = new THREE.OutlineEffect(renderer);
+
+    addLights();
+    addSky();
+    addDinosaur();
+    // addCloud();
+    addTree();
+
+    let dinosaurcanvas = document.getElementById('dinosaurdiv');
+    dinosaurcanvas.appendChild(renderer.domElement);
+
+    dinosaurcanvas.addEventListener('mousemove', onMouseMove, false);
+
+    window.addEventListener('resize', onWindowResize, false);
+}
+
+function addLights() {
+    const light = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(light);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    directionalLight.position.set(3, 10, 3);
+    directionalLight.castShadow = true;
+    scene.add(directionalLight);
+}
+
+function addDinosaur() {
+    dinosaur = new Dinosaur();
+    scene.add(dinosaur.group);
+}
+
+function addCloud() {
+    cloud = new Cloud();
+    scene.add(cloud.group);
+}
+
+function addTree() {
+    tree = new Tree();
+    scene.add(tree.group);
+}
+
+function addSky() {
+    sky = new Sky();
+    sky.showNight(night);
+    scene.add(sky.group);
+}
+
+function onMouseMove(event) {
+    event.preventDefault();
+
+    const x = event.clientX;
+    const y = event.clientY;
+    // rotate the scene if mouse is pressed
+    if (event.buttons === 1) {
+        scene.rotation.y = (x - width / 2) / width * 6;
+        scene.rotation.x = (y - height / 2) / height * 6;
+    }
+    // translate the scene if mouse is pressed
+    if (event.buttons === 2) {
+        scene.position.x = (x - width / 2) / width * 6;
+        scene.position.y = -(y - height / 2) / height * 6;
+    }
+}
+
+function onWindowResize() {
+    width = window.innerWidth - marginWidth;
+    height = window.innerHeight - marginHeight;
+
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(width, height);
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI / 180);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    dinosaur.update();
+    // cloud.update();
+    tree.update();
+    sky.update();
+
+    renderer.render(scene, camera);
+}
+
+class Dinosaur {
+    constructor() {
+        this.group = new THREE.Group();
+
+        this.skinMaterial = new THREE.MeshLambertMaterial({
+            color: 0x79bd69,
+            roughness: 1,
+            // wireframe: true,
+            flatShading: true
+        });
+        this.bellyMaterial = new THREE.MeshLambertMaterial({
+            color: 0xEFEFEF,
+            roughness: 1,
+            flatShading: true
+        });
+        this.hornMaterial = new THREE.MeshLambertMaterial({
+            color: 0xFFC0CB,
+            shininess: 1,
+            flatShading: true
+        });
+        this.eyeMaterial = new THREE.MeshLambertMaterial({
+            color: 0x8B4513,
+            shininess: 1,
+            flatShading: true
+        });
+        this.mouthMaterial = new THREE.MeshLambertMaterial({
+            color: 0x8B4513,
+            shininess: 1,
+            flatShading: true
+        });
+        this.cheekMaterial = new THREE.MeshLambertMaterial({
+            color: 0xeffc2c6,
+            shininess: 1,
+            flatShading: true
+        });
+
+        this.drawBody();
+        this.drawHead();
+        this.drawNeck();
+        this.drawHorn();
+        this.drawLeg();
+        this.drawArm();
+        this.drawTail();
+    }
+    drawBody() {
+        this.body = new THREE.Mesh(new THREE.SphereGeometry(1.1, 24, 24), this.skinMaterial);
+        this.body.castShadow = true;
+        this.body.receiveShadow = true;
+        this.group.add(this.body);
+
+        this.belly = new THREE.Mesh(new THREE.SphereGeometry(1, 20, 20), this.bellyMaterial);
+        this.belly.position.set(0, -0.08, 0.16);
+        this.belly.scale.set(0.96, 1.02, 1.04);
+        this.belly.castShadow = true;
+        this.belly.receiveShadow = true;
+        this.group.add(this.belly);
+    }
+    drawHead() {
+        this.head = new THREE.Mesh(new THREE.SphereGeometry(1, 16, 16), this.skinMaterial);
+        this.head.position.set(0, 1.4, 0.3);
+        this.head.scale.set(1, 1, 1.1);
+        this.head.castShadow = true;
+        this.head.receiveShadow = true;
+        this.group.add(this.head);
+
+        const leftEye = new THREE.Mesh(new THREE.SphereGeometry(0.12, 10, 10), this.eyeMaterial);
+        leftEye.position.set(-0.45, 0.25, 0.75);
+        leftEye.castShadow = true;
+        leftEye.receiveShadow = true;
+        this.head.add(leftEye);
+
+        const rightEye = leftEye.clone();
+        rightEye.position.x = -leftEye.position.x;
+        this.head.add(rightEye);
+
+        let mouth = [];
+        for (let i = 0; i < 10; i++) {
+            const mouthLine = new THREE.Mesh(new THREE.BoxGeometry(0.24, 0.08, 1.84), this.mouthMaterial);
+            mouthLine.position.set(0, 0, 0.1);
+            mouthLine.rotation.y = deg2rad(-4.5*8 + i * 8);
+            mouthLine.rotation.z = deg2rad(-45)+(i%2)*deg2rad(90);
+            mouth.push(mouthLine);
+        }
+        mouth.forEach((mouthLine) => {
+            this.head.add(mouthLine);
+        });
+
+        const leftCheek = new THREE.Mesh(new THREE.CylinderGeometry(0.16, 0.16, 0.22, 12), this.cheekMaterial);
+        leftCheek.position.set(-0.66, 0.18, 0.55);
+        leftCheek.rotation.x = deg2rad(90);
+        leftCheek.rotation.y = deg2rad(-15);
+        leftCheek.rotation.z = deg2rad(56);
+        leftCheek.castShadow = true;
+        leftCheek.receiveShadow = true;
+        this.head.add(leftCheek);
+
+        const rightCheek = leftCheek.clone();
+        rightCheek.position.x = -leftCheek.position.x;
+        rightCheek.rotation.y = -leftCheek.rotation.y;
+        rightCheek.rotation.z = -leftCheek.rotation.z;
+        this.head.add(rightCheek);
+    }
+    drawNeck() {
+        this.neck = new THREE.Mesh(new THREE.CylinderGeometry(0.7, 0.9, 1.2, 24), this.skinMaterial);
+        this.neck.position.set(0, 0.86, -0.08);
+        this.neck.rotation.x = deg2rad(3);
+        this.neck.castShadow = true;
+        this.neck.receiveShadow = true;
+        this.group.add(this.neck);
+    }
+    drawHorn() {
+        this.horn = new THREE.Mesh(new THREE.ConeGeometry(0.4, 0.3), this.hornMaterial);
+        this.horn.position.set(0, 2.43, 0.3);
+        this.horn.scale.set(0.5, 1, 1);
+        this.horn.castShadow = true;
+        this.horn.receiveShadow = true;
+        this.group.add(this.horn);
+
+        this.horn2 = this.horn.clone();
+        this.horn2.position.y = this.horn.position.y - 0.14;
+        this.horn2.position.z = this.horn.position.z-0.6;
+        this.horn2.rotation.x = deg2rad(-32);
+        this.group.add(this.horn2);
+
+        this.horn3 = this.horn2.clone();
+        this.horn3.position.y = this.horn2.position.y - 0.44;
+        this.horn3.position.z = this.horn2.position.z - 0.44;
+        this.horn3.rotation.x = deg2rad(-66);
+        this.group.add(this.horn3);
+
+        this.horn4 = this.horn3.clone();
+        this.horn4.scale.set(0.52, 1.2, 1.4);
+        this.horn4.position.y = this.horn3.position.y - 0.62;
+        this.horn4.position.z = this.horn3.position.z - 0.16;
+        this.horn4.rotation.x = deg2rad(-80);
+        this.group.add(this.horn4);
+
+        this.horn5 = this.horn4.clone();
+        this.horn5.position.y = this.horn4.position.y - 0.68;
+        this.horn5.position.z = this.horn4.position.z - 0.15;
+        this.horn5.rotation.x = deg2rad(-70);
+        this.group.add(this.horn5);
+
+        this.horn6 = this.horn5.clone();
+        this.horn6.position.y = this.horn5.position.y - 0.6;
+        this.horn6.position.z = this.horn5.position.z - 0.14;
+        this.horn6.rotation.x = deg2rad(-64);
+        this.group.add(this.horn6);
+
+        this.horn7 = this.horn6.clone();
+        this.horn7.position.y = this.horn6.position.y - 0.55;
+        this.horn7.position.z = this.horn6.position.z - 0.2;
+        this.horn7.rotation.x = deg2rad(-58);
+        this.group.add(this.horn7);
+
+    }
+    drawLeg() {
+        this.leftLeg = new THREE.Mesh(new THREE.CylinderGeometry(0.56, 0.3, 1.0, 16), this.skinMaterial);
+        this.leftLeg.position.set(-0.5, -0.7, 0.5);
+        this.leftLeg.rotation.x = deg2rad(-15);
+        this.leftLeg.rotation.z = deg2rad(-10);
+        this.leftLeg.castShadow = true;
+        this.leftLeg.receiveShadow = true;
+        this.group.add(this.leftLeg);
+
+        this.rightLeg = this.leftLeg.clone();
+        this.rightLeg.position.x = -this.leftLeg.position.x;
+        this.rightLeg.rotation.z = -this.leftLeg.rotation.z;
+        this.group.add(this.rightLeg);
+    }
+    drawArm() {
+        this.leftArm = new THREE.Mesh(new THREE.CylinderGeometry(0.2, 0.15, 0.5, 12), this.skinMaterial);
+        this.leftArm.position.set(0.72, 0.35, 0.65);
+        this.leftArm.rotation.x = deg2rad(-110);
+        this.leftArm.rotation.z = deg2rad(20);
+        this.leftArm.castShadow = true;
+        this.leftArm.receiveShadow = true;
+        this.group.add(this.leftArm);
+
+        this.leftHand = new THREE.Mesh(new THREE.SphereGeometry(0.156), this.skinMaterial);
+        this.leftHand.position.set(0.796, 0.4236, 0.847);
+        this.leftHand.castShadow = true;
+        this.leftHand.receiveShadow = true;
+        this.group.add(this.leftHand);
+
+        this.rightArm = this.leftArm.clone();
+        this.rightArm.position.x = -this.leftArm.position.x;
+        this.rightArm.rotation.z = -this.leftArm.rotation.z;
+        this.group.add(this.rightArm);
+
+        this.rightHand = this.leftHand.clone();
+        this.rightHand.position.x = -this.leftHand.position.x;
+        this.group.add(this.rightHand);
+    }
+    drawTail() {
+        this.tail = new THREE.Mesh(new THREE.CylinderGeometry(0.1, 0.86, 1.25, 20), this.skinMaterial);
+        this.tail.position.set(0, -0.77, -0.96);
+        this.tail.rotation.x = deg2rad(-126);
+        this.tail.castShadow = true;
+        this.tail.receiveShadow = true;
+        this.group.add(this.tail);
+
+        this.tail2 = new THREE.Mesh(new THREE.ConeGeometry(0.382, 0.84, 20), this.skinMaterial);
+        this.tail2.position.set(0, -1.055, -1.47);
+        this.tail2.rotation.x = deg2rad(-120);
+        this.tail2.castShadow = true;
+        this.tail2.receiveShadow = true;
+        this.group.add(this.tail2);
+    }
+    update() {
+        this.group.rotation.y += 0.01;
+    }
+}
+
+class Cloud {
+    constructor() {
+        this.group = new THREE.Group();
+
+        this.cloudMaterial = new THREE.MeshPhongMaterial({
+            color: 0xeeeeee,
+            shininess: 1,
+            flatShading: true
+        });
+
+        this.drawCloud();
+    }
+    drawCloud() {
+        this.cloud = new THREE.Mesh(new THREE.SphereGeometry(5, 4, 6), this.cloudMaterial);
+        this.cloud.castShadow = true;
+        this.cloud.receiveShadow = true;
+        this.group.add(this.cloud);
+
+        this.cloud2 = this.cloud.clone();
+        this.cloud2.scale.set(0.55, 0.35, 1);
+        this.cloud2.position.set(5, -1.5, 2);
+        this.group.add(this.cloud2);
+
+        this.cloud3 = this.cloud.clone();
+        this.cloud3.scale.set(0.75, 0.5, 1);
+        this.cloud3.position.set(-5.5, -2, -1);
+        this.group.add(this.cloud3);
+    }
+    update() {
+        this.group.rotation.y += 0.001;
+    }
+}
+
+class Sky {
+    constructor() {
+        this.group = new THREE.Group();
+
+        this.skyMaterial = new THREE.MeshPhongMaterial({
+            color: 0x00ffff,
+            shininess: 0,
+            flatShading: true
+        });
+
+        this.drawSky();
+    }
+    drawSky() {
+        this.sky = new THREE.Mesh(new THREE.SphereGeometry(100, 32, 32), this.skyMaterial);
+        this.sky.castShadow = false;
+        this.sky.receiveShadow = false;
+        this.group.add(this.sky);
+    }
+    showNight(night) {
+        if (night) {
+            this.skyMaterial.color.setHex(0x000000);
+        } else {
+            this.skyMaterial.color.setHex(0x00ffff);
+        }
+    }
+    update() {
+        this.group.rotation.y += 0.001;
+    }
+}
+
+class Tree {
+    constructor() {
+        this.group = new THREE.Group();
+
+        this.treeMaterial = new THREE.MeshPhongMaterial({
+            color: 0x04ee04,
+            shininess: 0,
+            flatShading: true
+        });
+        this.trunkMaterial = new THREE.MeshPhongMaterial({
+            color: 0x8B4513,
+            shininess: 0,
+            flatShading: true
+        });
+
+        this.drawTree();
+    }
+    drawTree() {
+        this.tree = new THREE.Mesh(new THREE.ConeGeometry(0.75, 0.6), this.treeMaterial);
+        this.tree.position.set(1.85, 0.25, 0.8);
+        this.tree.castShadow = true;
+        this.tree.receiveShadow = true;
+        this.group.add(this.tree);
+
+        this.tree2 = this.tree.clone();
+        this.tree2.scale.set(0.7, 1.1, 0.8);
+        this.tree2.position.set(1.85, 0.75, 0.8);
+        this.tree2.rotation.y = deg2rad(35);
+        this.group.add(this.tree2);
+
+        this.tree3 = this.tree.clone();
+        this.tree3.scale.set(0.5, 1.2, 0.6);
+        this.tree3.position.set(1.85, 1.25, 0.8);
+        this.group.add(this.tree3);
+
+        this.trunk = new THREE.Mesh(new THREE.CylinderGeometry(0.05, 0.08, 1.6, 12), this.trunkMaterial);
+        this.trunk.position.set(1.85, -0.4, 0.8);
+        this.trunk.castShadow = true;
+        this.trunk.receiveShadow = true;
+        this.group.add(this.trunk);
+    }
+    update() {
+        this.group.rotation.y += 0.01;
+    }
+}
+
+init();
+animate();
